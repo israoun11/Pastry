ca0052c26bbd01f24ed43bcdd85b852162b695b0
israoun11 <israoun55@gmail.com>
2026-08-24 19:14:57 +0100

    fix

--- api/config/db_Connect.js
+++ api/config/db_Connect.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.DB_URI);
 
     console.log("Database connected successfully");
-    console.log("DATABASE:", mongoose.connection.name);
   } catch (error) {
     console.error("Database connection error:", error);
     throw error;
