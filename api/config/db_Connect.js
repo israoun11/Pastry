2ff78b33f866ca2c86b47cca15c93746f7ad15b3
israoun11 <israoun55@gmail.com>
2026-08-24 16:04:52 +0100

    pastry

--- api/config/db_Connect.js
+++ api/config/db_Connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connString = process.env.MONGO_URI || process.env.DB_URI || "mongodb+srv://isra:isra123@cluster0.bygr3wq.mongodb.net/pastryDB?retryWrites=true&w=majority";
+    const connString = process.env.MONGO_URI || process.env.DB_URI || "mongodb+srv://isra:isra123@cluster0.bygr3wq.mongodb.net/isra'sproject";
     
     if (mongoose.connection.readyState >= 1) {
       return;
