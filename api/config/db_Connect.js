d596c6b0b31f62952439fddacac940c1e5e8a3e8
israoun11 <israoun55@gmail.com>
2026-08-24 18:16:24 +0100

    Back

--- api/config/db_Connect.js
+++ api/config/db_Connect.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const connectB = async () => {
   try {
-   
-
-    await mongoose.connect("mongodb+srv://isra:isra123@cluster0.bygr3wq.mongodb.net/isra'sproject");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection error:", error);
+    throw error;
   }
 };
 
-module.exports = connectDB;
+module.exports = connectB;
